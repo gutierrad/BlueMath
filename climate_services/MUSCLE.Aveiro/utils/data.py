94c341ce649eaded1576ea63f59a50df5b264415
Javier Tausía Hoyal <tausiaj@unican.es>
2025-03-31 20:10:15 +0000

    [JTH] add MUSCLE working in Aveiro zero to hero in devcontainer

--- climate_services/MUSCLE.Aveiro/utils/data.py
+++ climate_services/MUSCLE.Aveiro/utils/data.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
 
+import numpy as np
 import xarray as xr
 from bluemath_tk.core.operations import spatial_gradient
 from bluemath_tk.predictor.xwt import get_dynamic_estela_predictor
@@ -12,13 +13,21 @@ def load_atmospheric_predictor(
     Load atmospheric predictor data for the given region
     """
 
-    era5 = xr.open_dataset("data/era5_atmospheric.nc")
-    # era5 = era5.sel(
-    #     latitude=slice(region[3], region[1]), longitude=slice(region[0], region[2])
-    # )
+    era5 = xr.open_dataset("https://geoocean.sci.unican.es/thredds/dodsC/geoocean/era5-msl")
+    era5["time"] = era5["time"].astype("timedelta64[D]") + np.datetime64("1940-01-01")
+    era5 = (
+        era5
+        .sel(
+            latitude=slice(region[3], region[1], 3), 
+            longitude=slice(region[0], region[2], 3), 
+            time=slice("1980", None)
+        )
+        .load()
+        # .coarsen(longitude=2, latitude=2).mean()
+    )
 
-    # if "msl_gradient" in variables:
-    #     era5["msl_gradient"] = spatial_gradient(era5["msl"])
+    if "msl_gradient" in variables:
+        era5["msl_gradient"] = spatial_gradient(era5["msl"])
 
     if estela:
         estela = xr.open_dataset("data/estela_sea.nc")
