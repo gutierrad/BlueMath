1d499f58e79d78702cdcf33142f80755ca491d5b
valvanuz <valvanuz.fernandez@unican.es>
2025-05-29 20:02:14 +0200

    HySwash_Veggy working monochromatic

---
+++ methods/hybrid_downscaling/metamodels/HySwash/utils/RunSwash.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=Vsin_veg
+#SBATCH --output=output_%A_%a.err
+#SBATCH --error=error_%A_%a.err
+#SBATCH --partition=geocean_priority
+##SBATCH --nodelist=geocean01
+
+
+id=$(printf "%04d\n" "$SLURM_ARRAY_TASK_ID")
+folder="/home/grupos/geocean/valvanuz/HySwash/BlueMath/methods/hybrid_downscaling/metamodels/HySwash/output/$id"
+launchSwash.sh --case-dir $folder > $folder/wrapper_out.log 2> $folder/wrapper_err.log
---
+++ methods/hybrid_downscaling/metamodels/HySwash/utils/postprocess_parallel.py
@@ -0,0 +1,20 @@
+import os
+import os.path as op
+from bluemath_tk.core.io import load_model
+import sys
+
+case_num = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+
+root_dir="/home/grupos/geocean/valvanuz/HySwash/BlueMath/methods/hybrid_downscaling/metamodels/HySwash"
+#output_dir = "/lustre/geocean/DATA/hidronas1/valva/Veggy_topo_alba"
+output_dir = os.path.join(root_dir, "output")
+templates_dir = os.path.join(root_dir, "templates", "VeggyBig")
+export_dir = op.join(root_dir, "HyVeggy_exported")
+
+
+
+swash_model = load_model(op.join(export_dir, "swash_model.pkl"))
+swash_model.load_cases()
+#swash_model.build_cases([case_num])
+#swash_model.run_cases([case_num])
+swash_model.postprocess_cases([case_num])
